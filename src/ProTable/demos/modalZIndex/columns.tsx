6222137171cce976e5e101fad9611d8079378635
jay.kou <jay.kou@qq.com>
2024-04-16 16:33:50 +0800

    feat: 增加一个 protable 用例

---
+++ src/ProTable/demos/modalZIndex/columns.tsx
@@ -0,0 +1,47 @@
+import { LinkButton, TableColumnType } from 'react-admin-kit';
+
+export const getColumns = (): TableColumnType[] => [
+  {
+    title: '用户名',
+    dataIndex: 'name',
+    required: true,
+  },
+  {
+    title: '电话',
+    dataIndex: 'phone',
+  },
+  {
+    title: '昵称',
+    dataIndex: 'nickName',
+    hideInSearch: true,
+  },
+  {
+    title: '性别',
+    dataIndex: 'sex',
+    valueType: 'radio',
+    fieldProps: {
+      options: [
+        { label: '男', value: '男' },
+        { label: '女', value: '女' },
+      ],
+    },
+  },
+  {
+    title: '身份证号',
+    dataIndex: 'idNumber',
+    hideInSearch: true,
+  },
+  {
+    title: '操作',
+    valueType: 'option',
+    enableDelete: true,
+    render: (text, record, index, actionRef, innerRef) => [
+      <LinkButton
+        key={1}
+        onClick={() => innerRef.current?.openModal('edit', record)}
+      >
+        编辑
+      </LinkButton>,
+    ],
+  },
+];
