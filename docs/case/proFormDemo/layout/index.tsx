822b77ae972c4835bf17cb113b0672e9517b642e
jay.kou <jay.kou@qq.com>
2024-05-20 17:30:17 +0800

    doc: 增加 formList 内嵌的布局案例

--- docs/case/proFormDemo/layout/index.tsx
+++ docs/case/proFormDemo/layout/index.tsx
@@ -7,22 +7,17 @@ export default function () {
         embed
         columns={[{ title: '用户', dataIndex: 'user' }, { title: '密码' }]}
       />
-      <SchemaForm
-        layout="vertical"
-        embed
-        columns={[{ title: '用户', dataIndex: 'user2' }, { title: '密码' }]}
-      />
 
       <SchemaForm
+        embed
+        grid
+        initialValues={{ shopList: [{}] }}
         columns={[
           {
             dataIndex: 'shopList',
             valueType: 'formList',
-            colProps: { span: 24 },
-            rowProps: { gutter: [8, 0] },
+
             fieldProps: {
-              className: 'jayjay',
-              id: 'jayid',
               alwaysShowItemLabel: true,
               creatorButtonProps: {
                 creatorButtonText: '添加店铺',
@@ -30,34 +25,39 @@ export default function () {
             },
             columns: [
               {
-                title: '店铺',
-                dataIndex: 'shop',
-                required: true,
-                colProps: { span: 16 },
-              },
-              {
-                title: '店铺面积',
-                dataIndex: 'area',
-                valueType: 'digit',
-                colProps: { span: 8 },
-                fieldProps: { style: { width: '100%' } },
-              },
-              {
-                title: '店铺联系人',
-                dataIndex: 'contacts',
-                colProps: { span: 12 },
-              },
-              {
-                title: '联系人电话',
-                dataIndex: 'contactsPhone',
-                colProps: { span: 12 },
+                valueType: 'group',
+                rowProps: { gutter: [8, 0] },
+                columns: [
+                  {
+                    title: '店铺',
+                    dataIndex: 'shop',
+                    required: true,
+                    colProps: { span: 16 },
+                  },
+                  {
+                    title: '店铺面积',
+                    dataIndex: 'area',
+                    valueType: 'digit',
+                    colProps: { span: 8 },
+                    fieldProps: { style: { width: '100%' } },
+                  },
+                  {
+                    title: '店铺联系人',
+                    dataIndex: 'contacts',
+                    colProps: { span: 12 },
+                  },
+                  {
+                    title: '联系人电话',
+                    dataIndex: 'contactsPhone',
+                    colProps: { span: 12 },
+                  },
+                ],
               },
             ],
           },
         ]}
         autoFocusFirstInput={false}
-        grid
-        embed
+        // grid
         colProps={{ span: 24 }}
       />
     </ProForm>
