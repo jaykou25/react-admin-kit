d5eac2568dd5dadbd683db42dd4d84e282c37728
jay.kou <jay.kou@qq.com>
2024-08-21 09:17:44 +0800

    doc: 更改 slogon

--- .dumi/pages/index.tsx
+++ .dumi/pages/index.tsx
@@ -34,7 +34,7 @@ const Home = () => {
             </span>
           </span>
         </h1>
-        <div className="slogon">开发中后台应用的最佳伴侣</div>
+        <div className="slogon">中后台应用的最佳伴侣</div>
         <p className="description">构建高质量的表单和表格</p>
         <p className="actions">
           <Link className="get-started" to="/components/schema-form">
