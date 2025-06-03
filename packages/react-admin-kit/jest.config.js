6efc9d0a6e65030b1202acd93c09e49068e134e8
jay.kou <jay.kou@qq.com>
2025-06-03 10:45:34 +0800

    feat: 增加测试覆盖率

--- packages/react-admin-kit/jest.config.js
+++ packages/react-admin-kit/jest.config.js
@@ -12,4 +12,25 @@ module.exports = {
       '@ant-design/pro-form/lib/components/SchemaForm/valueType/index',
     '.(css|less)$': 'identity-obj-proxy',
   },
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/demos/**/*', // 排除 demos
+    '!src/**/cases/**/*', // 排除 cases
+    '!src/**/*.d.ts', // 排除类型声明文件
+    '!src/**/types.ts', // 排除类型声明文件
+    '!src/**/genItems.tsx', // 排除辅助文件
+    '!src/index.ts', // 排除入口文件
+    '!src/setupTests.ts', // 排除测试工具文件
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 85,
+      lines: 90,
+      statements: 90,
+    },
+  },
+  coverageReporters: ['html', 'text', 'lcov'],
 };
